16abe5d82e242cc737e738be7ad978afd34aa7d9
christian hofer <christian.hofer@student.uibk.ac.at>
2018-04-18 18:03:57 +0200

    verschiedene basemaps

--- basemap.js
+++ basemap.js
@@ -1,6 +1,18 @@
 
 let myMap = L.map("mapdiv");
-let myLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+let myLayers = {
+    osm: L.tileLayer ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+    geolandbasemap: L.tileLayer ("https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png"),
 
-myMap.addLayer(myLayer);
+    bmapoverlay: L.tileLayer ("https://maps.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png"),
+
+    bmapgrau: L.tileLayer ("https://maps.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png"),
+
+    bmaphidpi: L.tileLayer ("https://maps.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg"),
+
+    bmaporthofoto30cm: L.tileLayer ("https://maps.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg"),
+
+}
+
+myMap.addLayer(myLayers.osm);
 myMap.setView([47.267,11.383], 11);
