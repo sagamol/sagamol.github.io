7da0e18630b8d971e237c41970757707cc669c90
sagamol <christian.hofer@student.uibk.ac.at>
2018-05-17 12:02:13 +0200

    Wl8 fertiggestellt

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -81,85 +81,66 @@ const myLayers = {
 
 // Layergruppen für die Elektronische Karte Tirol definieren
 const tirisSommer = L.layerGroup([
-    grundkartenLayer.eKarte_Tirol_Sommer,
-    grundkartenLayer.gdi_nomenklatur
+    myLayers.eKarte_Tirol_Sommer,
+    myLayers.gdi_nomenklatur
 ]);
 const tirisWinter = L.layerGroup([
-    grundkartenLayer.eKarte_Tirol_Winter,
-    grundkartenLayer.gdi_nomenklatur
+    myLayers.eKarte_Tirol_Winter,
+    myLayers.gdi_nomenklatur
 ]);
 const tirisOrtho = L.layerGroup([
-    grundkartenLayer.eKarte_Tirol_Ortho,
-    grundkartenLayer.gdi_nomenklatur
+    myLayers.eKarte_Tirol_Ortho,
+    myLayers.gdi_nomenklatur
 ]);
 
-myMap.addLayer(myLayers.geolandbasemap);
-myMap.addLayer(etappe12group);
+// Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
 let myMapControl = L.control.layers({
     "Openstreetmap": myLayers.osm,
     "basemap.at Grundkarte": myLayers.geolandbasemap,
-    //"basemap.at grau": myLayers.bmapgrau,
-    //"basemap.at highdpi": myLayers.bmaphidpi,
-    //"basemap.at Orthofoto": myLayers.bmaporthofoto30cm,
-    "gdi_base_summer": myLayers.eKarte_Tirol_Sommer,
-    "gdi_base_winter": myLayers.eKarte_Tirol_Winter,
-    "gdi_ortho": myLayers.eKarte_Tirol_Ortho,
+    "basemap.at grau": myLayers.bmapgrau,
+    "basemap.at highdpi": myLayers.bmaphidpi,
+    "basemap.at Orthofoto": myLayers.bmaporthofoto30cm,
+    "Elektronische Karte Tirol - Sommer": tirisSommer,
+    "Elektronische Karte Tirol - Winter": tirisWinter,
+    "Elektronische Karte Tirol - Orthophoto": tirisOrtho,
 }, {
-        "basemap.at Overlay": myLayers.bmapoverlay,
-        "Variante Pillersee": etappe12group,
-        //"Beschriftung eKarte Tirol": gdi_nomenklatur,
+        "Etappe12: Variante Pillersee": etappe12group,
+        "Start / Ziel": overlayMarker,
     });
-myMap.addControl(myMapControl);
 
+myMap.addControl(myMapControl);
+myMap.addLayer(myLayers.geolandbasemap);
 myMap.setView([47.528115, 12.577668], 11);
 
-//let geojson = L.geoJSON(etappe12data).addTo(etappe12group);
-const geojson = L.geoJSON(etappe12data, {
-    type: "LineString",
-    coordinates: [[47.570246, 12.468518][ 47.569861, 12.46965 ], [ 47.570421, 12.470765 ]],
-}).addTo(etappe12group);
-
+// Maßstabsleiste metrisch
 L.control.scale({           
     maxWidth : 200,        
     metric : true,          
     imperial : false,      
     position : "bottomleft" 
-
-}).addTo(myMap);           
-
-const etappenbeginnkoor = [47.570246,12.468518];
-const etappenendekoor = [47.501216,12.4901];
-
-let etappenbeginnMarker = L.marker(etappenbeginnkoor).addTo(etappe12group);
-etappenbeginnMarker.bindPopup('<p>Erpfendorf</p><a href="https://www.kitzbueheler-alpen.com/de/erpfendorf/erpfendorf-tirol-ortsinformation-zahlen-fakten.html"> Erpfendorf in Tirol - Ortsinformation</a>');
-
-let myIcon1 = L.icon({
-    iconUrl: 'start-race-2.png',
-    });
-L.marker([47.570246,12.468518], {icon: myIcon1}).addTo(myMap);
-
-
-let etappenendeMarker = L.marker(etappenendekoor).addTo(etappe12group);
-etappenendeMarker.bindPopup('<p>Mühlbach</p><a href="https://de.wikipedia.org/wiki/M%C3%BChlbach_am_Hochk%C3%B6nig"> Mühlbach am Hochkönig</a>');
-
-let myIcon2 = L.icon({
-  iconUrl: 'finish.png',
-  });
-L.marker([47.501216,12.4901], {icon: myIcon2}).addTo(myMap);
-
-myMap.addLayer(etappe12group)
-
-
-
-// WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
-
-// Maßstab metrisch ohne inch
+}).addTo(myMap);
 
 // Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
+L.marker([47.570246,12.468518],{
+    icon : L.icon({
+        iconUrl : 'start-race-2.png',
+        iconAnchor : [16,37],
+        popupAnchor : [0,-37],
+    })
+}).bindPopup(
+    '<h3>Erpfendorf</h3><p><a href="https://www.kitzbueheler-alpen.com/de/erpfendorf/erpfendorf-tirol-ortsinformation-zahlen-fakten.html"> Erpfendorf in Tirol - Ortsinformation</a></p>'
+).addTo(overlayMarker);
+
+L.marker([47.501216,12.4901],{
+    icon : L.icon({
+        iconUrl : 'finish.png',
+        iconAnchor : [16,37],
+        popupAnchor : [0,-37],
+    })
+}).bindPopup(
+    '<h3>Mühlbach</h3><p><a href="https://de.wikipedia.org/wiki/M%C3%BChlbach_am_Hochk%C3%B6nig"> Mühlbach am Hochkönig</a></p>'
+).addTo(overlayMarker);
 
 // GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
-// Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
-
-// Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
-
-// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
+let geojsonTrack = L.geoJSON(etappe12data).addTo(etappe12group);
+myMap.fitBounds(geojsonTrack.getBounds())
